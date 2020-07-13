73a0d95ffb7a633ed6dbe46f4270b92b5ff986c9
Sumay Thakurdesai <sumaythakurdesai@MacBook-Pro.local>
2020-07-13 12:54:43 -0700

    added function to compute integral images

--- facedetection.py
+++ facedetection.py
@@ -1,6 +1,7 @@
 # importing libraries
 import cv2
 import os
+import numpy as np
 
 # function going through every directory in test images
 def build_train_data(faces, non_faces):
@@ -24,14 +25,32 @@ def build_train_data(faces, non_faces):
                   non_faces_list.append(img)
    return faces_list, non_faces_list
 
+# gets integral images
+def get_integral_imgs(images):
+   integral_imgs = []
+   for image in images:
+      img_shape = image.shape
+      integral_img = np.empty(img_shape, dtype=int)
+      for i in range(img_shape[0]):
+         for j in range(img_shape[1]):
+            if (i, j) == (0, 0):
+               integral_img[i, j] = image[i, j]
+            elif i == 0:
+               integral_img[i, j] = integral_img[i, j - 1] + image[i, j]
+            elif j == 0:
+               integral_img[i, j] = integral_img[i - 1, j] + image[i, j]
+            else:
+               integral_img[i, j] = image[i, j] + integral_img[i - 1, j] + integral_img[i, j - 1] - integral_img[
+                  i - 1, j - 1]
 
+      integral_imgs.append(integral_img)
+      return np.asarray(integral_imgs)
 
 
 def main():
    faces_list, non_faces_list = build_train_data('./testimgs/test1', './testimgs/test2')
-   print(
-      len(faces_list) + len(non_faces_list)
-   )
+   faces = get_integral_imgs(faces_list)
+   non_faces = get_integral_imgs(non_faces_list)
 
 
 if __name__ == "__main__":
